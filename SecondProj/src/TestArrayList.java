831f2cbc75b8a94d607e796edf376226adc607b4
atika95 <noratikah189@gmail.com>
2018-02-12 16:38:54 +0800

    new modify

--- SecondProj/src/TestArrayList.java
+++ SecondProj/src/TestArrayList.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -13,7 +15,26 @@ public class TestArrayList {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList al = new ArrayList<>();
+		
+		for(String str:args) {
+			al.add(str);
+		}	
+		System.out.println("Array List is empty: " + al.isEmpty());
+		//al.add(new Integer(10));
+		//al.add(Boolean.TRUE);
+		
+		System.out.println("original :" + al);
+		al.remove(" Two ");
+		System.out.println("Now :" + al);
+		al.set(1, "New");
+		System.out.println("Contains four :" + al.contains("four"));
+		al.clear();
+		System.out.println("Now :" + al);
+		
+			
+			
+		}
 	}
 
-}
+
