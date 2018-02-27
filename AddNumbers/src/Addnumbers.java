e0332c6aa28da417e1d8c2194693d4f50957d5ca
atika95 <noratikah189@gmail.com>
2018-02-27 09:35:06 +0800

    exercise

---
+++ AddNumbers/src/Addnumbers.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author User
+ *
+ */
+public class Addnumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 2 * 4;
+		double y = 9/2;
+		double z = x+y;
+		
+		 System.out.println("The value  of x is:" + x);
+		 System.out.println("The value of y is:" + y);
+		 System.out.println("The total value is:" + z);
+	}
+
+}
